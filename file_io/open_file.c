18bf9e5eb22235a1bd06f4d43731039e7f94369f
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-09-26 03:57:10 +0900

    open function

---
+++ file_io/open_file.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+
+	int fd;
+	const char * file_name = "./sample.txt";
+
+	fd = open (file_name, O_RDONLY);
+
+	if (fd == -1)
+		puts("open error");
+	else
+		puts("open success");
+
+
+	if (close(fd) == -1)
+		perror ("close");
+
+	return 0;
+}
