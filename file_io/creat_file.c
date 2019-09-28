b08f98fd9198926e6da98d62ff3c0c3728bde86d
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-09-28 22:04:59 +0900

    creat function

---
+++ file_io/creat_file.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+
+	int fd;
+	const char * file_name = "./sample.txt";
+
+	fd = creat (file_name, 0644);
+
+	if (fd == -1)
+		puts("create error");
+	else
+		puts("create success");
+
+
+	if (close(fd) == -1)
+		perror ("close");
+
+	return 0;
+}
