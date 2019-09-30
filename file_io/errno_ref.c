4e7ab4e35b218a73470f1aada81bbbcfbc0f2c6a
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-10-01 02:33:04 +0900

    added errno referece file

---
+++ file_io/errno_ref.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <errno.h>
+
+int main() {
+	printf("EPERM:   %d\n", EPERM);
+	printf("ENOENT:  %d\n", ENOENT);
+	printf("ESRCH:   %d\n", ESRCH);
+	printf("EINTR:   %d\n", EINTR);
+	printf("EIO:     %d\n", EIO);
+	printf("ENXIO:   %d\n", ENXIO);
+	printf("E2BIG:   %d\n", E2BIG);
+	printf("ENOEXEC: %d\n", ENOEXEC);
+	printf("EBADF:   %d\n", EBADF);
+	printf("ECHILD:  %d\n", ECHILD);
+	printf("EAGAIN:  %d\n", EAGAIN);
+	printf("ENOMEM:  %d\n", ENOMEM);
+	printf("EACCES:  %d\n", EACCES);
+	printf("EFAULT:  %d\n", EFAULT);
+	printf("EBUSY:   %d\n", EBUSY);
+	printf("EEXIST:  %d\n", EEXIST);
+	printf("EXDEV:   %d\n", EXDEV);
+	printf("ENODEV:  %d\n", ENODEV);
+	printf("ENOTDIR: %d\n", ENOTDIR);
+	printf("EISDIR:  %d\n", EISDIR);
+	printf("EINVAL:  %d\n", EINVAL);
+	printf("ENFILE:  %d\n", ENFILE);
+	printf("EMFILE:  %d\n", EMFILE);
+	printf("ENOTTY:  %d\n", ENOTTY);
+	printf("ETXTBSY: %d\n", ETXTBSY);
+	printf("EFBIG:   %d\n", EFBIG);
+	printf("ENOSPC:  %d\n", ENOSPC);
+	printf("ESPIPE:  %d\n", ESPIPE);
+	printf("EROFS:   %d\n", EROFS);
+	printf("EMLINK:  %d\n", EMLINK);
+	printf("EPIPE:   %d\n", EPIPE);
+	printf("EDOM:    %d\n", EDOM);
+	printf("ERANGE:  %d\n", ERANGE);
+
+
+/*
+	printf("E2BIG:   %d\n", E2BIG);
+	printf("EACCES:  %d\n", EACCES);
+	printf("EAGAIN:  %d\n", EAGAIN);
+	printf("EBADF:   %d\n", EBADF);
+	printf("EBUSY:   %d\n", EBUSY);
+	printf("ECHILD:  %d\n", ECHILD);
+	printf("EDOM:    %d\n", EDOM);
+	printf("EEXIST:  %d\n", EEXIST);
+	printf("EFAULT:  %d\n", EFAULT);
+	printf("EFBIG:   %d\n", EFBIG);
+	printf("EINTR:   %d\n", EINTR);
+	printf("EINVAL:  %d\n", EINVAL);
+	printf("EIO:     %d\n", EIO);
+	printf("EISDIR:  %d\n", EISDIR);
+	printf("EMFILE:  %d\n", EMFILE);
+	printf("EMLINK:  %d\n", EMLINK);
+	printf("ENFILE:  %d\n", ENFILE);
+	printf("ENODEV:  %d\n", ENODEV);
+	printf("ENOENT:  %d\n", ENOENT);
+	printf("ENOEXEC: %d\n", ENOEXEC);
+	printf("ENOMEM:  %d\n", ENOMEM);
+	printf("ENOSPC:  %d\n", ENOSPC);
+	printf("ENOTDIR: %d\n", ENOTDIR);
+	printf("ENOTTY:  %d\n", ENOTTY);
+	printf("ENXIO:   %d\n", ENXIO);
+	printf("EPERM:   %d\n", EPERM);
+	printf("EPIPE:   %d\n", EPIPE);
+	printf("ERANGE:  %d\n", ERANGE);
+	printf("EROFS:   %d\n", EROFS);
+	printf("ESPIPE:  %d\n", ESPIPE);
+	printf("ESRCH:   %d\n", ESRCH);
+	printf("ETXTBSY: %d\n", ETXTBSY);
+	printf("EXDEV:   %d\n", EXDEV);
+*/
+	return 0;
+}
