24634c5e94e5e7f953058f1cca1c80aa55a3e3f9
taewoo <taewoo.kim.f.r@gmail.com>
2020-11-07 17:59:42 +0000

    added api ref - pread, pwrite

--- ref/api_ref.c
+++ ref/api_ref.c
@@ -38,6 +38,16 @@ int close (int fd);
 
 off_t lseek (int fd, off_t pos, int origin);
 /* ---------------------------------------------------------------------- */
+#define _XOPEN_SOURCE 500
+
+#include <unistd.h>
+
+ssize_t pread (int fd, void *buf, size_t count, off_t pos);
 /* ---------------------------------------------------------------------- */
+#define _XOPEN_SOURCE 500
+
+#include <unistd.h>
+
+ssize_t pwrite (int fd, const void *buf, size_t count, off_t pos);
 /* ---------------------------------------------------------------------- */
 /* ---------------------------------------------------------------------- */
